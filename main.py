aecc93eb2ec5cda3d8de098e47fc0fb8547d5911
Lewey <dlmelchor12@gmail.com>
2023-04-26 00:42:33 -0700

    built game window
    chose simple colors to use for the game and built the display window

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import pygame
 import numpy as np
 import time
+
+
+# COLORS:
+BG_COLOR = (10, 10, 10)
+GRID_COLOR = (40, 40, 40)
+DIE_NEXT_COLOR = (170, 170, 170)
+ALIVE_NEXT_COLOR = (255, 255, 255)
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+
+    running = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        screen.fill(GRID_COLOR)
+
+        if running:
+            pygame.display.update()
+
+        time.sleep(.001)
+
+if __name__ == '__main__':
+    main()
